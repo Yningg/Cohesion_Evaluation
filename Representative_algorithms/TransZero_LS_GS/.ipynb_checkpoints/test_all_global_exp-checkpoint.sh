9a2ba853cc55b749b93a75f399f5f59c51ba4868
Yningg <yning.zh@gmail.com>
2025-01-13 16:21:48 +0800

    del unnecessary files

--- Representative_algorithms/TransZero_LS_GS/.ipynb_checkpoints/test_all_global_exp-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-nohup python accuracy_globalsearch_exp.py --dataset BTW17 >> ./logs/test_all_global_exp.txt 2>&1 &&
-python accuracy_globalsearch_exp.py --dataset Chicago_COVID >> ./logs/test_all_global_exp.txt 2>&1 &&
-python accuracy_globalsearch_exp.py --dataset Crawled_Dataset144 >> ./logs/test_all_global_exp.txt 2>&1 &&
-python accuracy_globalsearch_exp.py --dataset Crawled_Dataset26  >> ./logs/test_all_global_exp.txt 2>&1 &
--- Representative_algorithms/TransZero_LS_GS/.ipynb_checkpoints/training_all_exp-checkpoint.sh
+++
@@ -1,8 +0,0 @@
-
-nohup  python link_pretrain_exp.py --dataset BTW17 --model_name BTW17 --batch_size 7721 --epochs 100 --dropout 0.1 --hidden_dim 128 --hops 5  --n_heads 8 --n_layers 1 --pe_dim 5 --peak_lr 0.001  --weight_decay=1e-05 --device 0 >> ./logs/BTW17_training.txt 2>&1 &&
-
-python link_pretrain_exp.py --dataset Chicago_COVID --model_name Chicago_COVID --batch_size 4971 --epochs 100 --dropout 0.1 --hidden_dim 128 --hops 5  --n_heads 8 --n_layers 1 --pe_dim 5 --peak_lr 0.001  --weight_decay=1e-05 --device 0 >> ./logs/Chicago_COVID_training.txt 2>&1 &&
-
-python link_pretrain_exp.py --dataset Crawled_Dataset144 --model_name Crawled_Dataset144 --batch_size 10388 --epochs 100 --dropout 0.1 --hidden_dim 128 --hops 5  --n_heads 8 --n_layers 1 --pe_dim 5 --peak_lr 0.001  --weight_decay=1e-05 --device 0 >> ./logs/Crawled_Dataset144_training.txt 2>&1 &&
-
-python link_pretrain_exp.py --dataset Crawled_Dataset26 --model_name Crawled_Dataset26 --batch_size 21236 --epochs 100 --dropout 0.1 --hidden_dim 128 --hops 5  --n_heads 8 --n_layers 1 --pe_dim 5 --peak_lr 0.001  --weight_decay=1e-05 --device 0 >> ./logs/Crawled_Dataset26_training.txt 2>&1 &
