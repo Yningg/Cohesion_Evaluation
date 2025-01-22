a95ccec4f61c523d622a4f039776ff90b2a69919
Yningg <yning.zh@gmail.com>
2025-01-22 22:41:54 +0800

    Improve the visualization

--- Visualization/Case_study.py
+++ Visualization/Case_study.py
@@ -16,14 +16,28 @@ target_path = "./"
 sys.path.append(target_path)
 import Cohesiveness_Calculation.General_function as gf
 
-
+def parse_cohesiveness_dim(cohesiveness_str):
+    # Remove the brackets and split the string by commas
+    cohesiveness_str = cohesiveness_str.strip('[]')
+    parts = cohesiveness_str.split(',')
+    
+    # Convert each part to a float, handling 'nan' values
+    cohesiveness_dim = []
+    for part in parts:
+        part = part.strip()
+        if part == 'nan':
+            cohesiveness_dim.append(0)
+        else:
+            cohesiveness_dim.append(float(part))
+    
+    return cohesiveness_dim
 
 # Only save the results with valid community nodes
 def process_algo_results(algo_result_dir, algorithm, file):
     results = {}
     with open(algo_result_dir + file, 'r') as f:
         lines = f.readlines()
-        if algorithm == ["ALS", "WCF-CRC", "I2ACSM"]:
+        if algorithm in ["ALS", "WCF-CRC", "I2ACSM"]:
             for line in lines:
                 parts = line.strip().split("\t")
                 node = str(parts[0])
@@ -32,10 +46,10 @@ def process_algo_results(algo_result_dir, algorithm, file):
                 community_node_list = list(ast.literal_eval(parts[3]))
                 community_node_list = [str(node) for node in community_node_list]
                 if len(community_node_list) > 0:
-                    coheisveness_dim = ast.literal_eval(parts[4])
+                    cohesiveness_dim = parse_cohesiveness_dim(parts[4])
                     if node not in results:
                         results[node] = []
-                    results[node].append([node, value, params, community_node_list, coheisveness_dim])
+                    results[node].append([node, value, params, community_node_list, cohesiveness_dim])
             
         elif algorithm in ["ST-Exa", "CSD", "Repeeling"]:
             for line in lines:
@@ -46,10 +60,10 @@ def process_algo_results(algo_result_dir, algorithm, file):
                 community_node_list = [str(node) for node in community_node_list]
                 
                 if len(community_node_list) > 0:
-                    coheisveness_dim = ast.literal_eval(parts[3])
+                    cohesiveness_dim = ast.literal_eval(parts[3])
                     if node not in results:
                         results[node] = []
-                    results[node].append([node, params, community_node_list, coheisveness_dim])
+                    results[node].append([node, params, community_node_list, cohesiveness_dim])
                     
         elif algorithm == "TransZero_LS":
             for line in lines:
@@ -57,10 +71,10 @@ def process_algo_results(algo_result_dir, algorithm, file):
                 node = str(parts[0])
                 community_node_list = ast.literal_eval(parts[1])
                 if len(community_node_list) > 0:
-                    coheisveness_dim = ast.literal_eval(parts[2]) 
+                    cohesiveness_dim = ast.literal_eval(parts[2]) 
                     if node not in results:
                         results[node] = []
-                    results[node].append([node, community_node_list, coheisveness_dim])
+                    results[node].append([node, community_node_list, cohesiveness_dim])
                 
     return results
 
@@ -98,20 +112,38 @@ def find_community(results, query_nodes):
 
 
 def print_query_community(node_community, algorithm_dict):
+    # Store communities with valid size by query node
+    valid_community = {}
     for query_node, algo_results in node_community.items():
-        print(f"Query node: {query_node}")
+        valid_community[query_node] = {}
         for algo, result in algo_results.items():
-            print(f"Algorithm: {algo}")
+            valid_community[query_node][algo] = []
             for single_result in result:
-                print(f"Community size: {len(single_result[algorithm_dict[algo]])}")
+                single_size = len(single_result[algorithm_dict[algo]])
+                if 5 <= single_size <= 30:
+                    valid_community[query_node][algo].append(single_result)
             print("\n")
+    
+    # Print the communities with valid size by query node
+    for query_node, algo_results in valid_community.items():
+        flag = [len(result) > 0 for result in algo_results.values()]
+        valid_algo_num = [1 for f in flag if f]
+        if np.sum(valid_algo_num) >= 3:
+            print(f"Query node: {query_node}")
+            for algo, result in algo_results.items():
+                print(f"Algorithm: {algo}, Number of communities: {len(result)}")
+                for single_result in result:
+                    print(f"Community size: {len(single_result[algorithm_dict[algo]])}")
+                    print(f"Community nodes: {single_result[algorithm_dict[algo]]}")
+                    print(f"Cohesiveness score: {single_result[-1]}")
+            print("=====================================")
 
 
 def plot_network_with_background(combined_graph, graph, community, title, pos, node_color, edge_color, background_node_color, background_edge_color):
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(8, 6))
     
     # Draw the background
-    nx.draw(combined_graph, pos=pos, node_color=background_node_color, edge_color=background_edge_color, with_labels=False, alpha=0.3)
+    nx.draw(combined_graph, pos=pos, node_color=background_node_color, node_size=30, edge_color=background_edge_color, with_labels=False, alpha=0.3)
     
      # Calculate edge widths based on the number of interactions and apply logarithmic scaling
     edge_widths = [graph.number_of_edges(u, v) for u, v in graph.edges()]
@@ -119,18 +151,20 @@ def plot_network_with_background(combined_graph, graph, community, title, pos, n
     max_log_width = max(log_edge_widths) if log_edge_widths else 1  # Avoid division by zero
     normalized_edge_widths = [width / max_log_width * 5 for width in log_edge_widths]  # Scale factor for visibility
     
-    # Draw the community nodes and edges with normalized edge widths
-    nx.draw(graph, pos=pos, node_color=node_color, edge_color=edge_color, width=normalized_edge_widths, with_labels=False)
+    # Set node colors
+    node_colors = ['red' if node == "1099134757" else node_color for node in graph.nodes()]
     
+    # Draw the community nodes and edges with normalized edge widths
+    nx.draw(graph, pos=pos, node_color=node_colors, node_size=30, edge_color=edge_color, width=normalized_edge_widths, with_labels=False)
     
     # Draw the community node labels with mapped numbers
     labels = {node: node_mapping[int(node)] for node in community}
     label_pos = {node: (x, y - 0.05) for node, (x, y) in pos.items()}  # Adjust the y-coordinate to position the label below the node
-    nx.draw_networkx_labels(graph, label_pos, labels=labels, font_color='black', verticalalignment='top')
+    nx.draw_networkx_labels(graph, label_pos, labels=labels, font_color='black', font_size=12, verticalalignment='top')
     
     # plt.show()
     plt.tight_layout()
-    plt.savefig("D:/Cohesion_Evaluation/Figures/Case_Study/" + title + ".png")
+    plt.savefig("D:/Cohesion_Evaluation/Figures/Case_Study/" + title + ".png", dpi=300, bbox_inches='tight')
 
 
 """
@@ -162,25 +196,26 @@ if __name__ == "__main__":
     dataset = "Chicago_COVID"
     algo_list = ["ALS", "WCF-CRC", "CSD", "ST-Exa", "Repeeling", "I2ACSM", "TransZero_LS"]
 
-    # Set the result directory
+    # Set directory
     result_dir = "D:/Cohesion_Evaluation/Cohesiveness_Output/"
-
+    query_dir = "D:/Cohesion_Evaluation/Original_Datasets/Query_Nodes/"
+    
 
     # Set the loc of community nodes is each algo
     algorithm_dict = {"ALS": 3, "WCF-CRC": 3, "CSD": 2, "ST-Exa": 2, "Repeeling": 2, "I2ACSM": 3, "TransZero_LS": 1}
 
-
     # Read the node mapping file
     node_mapping_file = "D:/Cohesion_Evaluation/Original_Datasets/Node_Mapping/Chicago_COVID_node_mapping.txt"
     node_mapping = gf.read_node_mapping(node_mapping_file)
+    # Reverse the mapping
+    node_mapping = {v: k for k, v in node_mapping.items()}
 
     # Read attribute file
     attribute_file = "D:/Cohesion_Evaluation/Original_Datasets/Preprocessed_Datasets/Chicago_COVID_attributed.txt"
     reply_G = gf.graph_construction(attribute_file)
 
     # Read query node file
-    query_node_file = "D:/Cohesion_Evaluation/Original_Datasets/Query_Nodes/Chicago_COVID_query_node.txt"
-    # query_nodes = gf.read_query_nodes(query_node_file)
+    # query_nodes = gf.get_query_nodes(query_dir, dataset)
 
     # Read the results of all algorithms
     # total_results = load_results(dataset, result_dir, algo_list)
@@ -197,26 +232,26 @@ if __name__ == "__main__":
 
 
     # WCF-CRC Community: reliability score: 0.3371472158657513 parameter: [0.0, 2, 4, 6], for communities with same size, no matter how to change the parameter, the community and cohesiveness score is the same
-    CRC_community = ['1047977523320160384', '3403201023', '387676464', '1089495612', '2579503212', '195639161', '261514017', '826495641983528960', '1132020458868609024', '875179181331673089', '26174781', '101868197', '1140372799594389632', '26708318', '59253988', '1156006445114171392', '232785287', '1055847424747622400', '1043676244771053568', '1032433488560054400', '97613792', '105581789', '120677834', '1099134757', '114170205', '22578954']
-    CRC_cohesiveness = [0.040675335086251395, 0.040675335086251395, 0.020210775983764067, 0.42924528301886794, 2.8]
+    CRC_community = ['1043676244771053568', '261514017', '120677834', '26708318', '1099134757', '101868197', '1140372799594389632', '875179181331673089', '3403201023', '114170205', '1032433488560054400', '1047977523320160384', '232785287', '2579503212', '1089495612', '826495641983528960', '59253988', '195639161', '387676464', '105581789', '1055847424747622400', '22578954', '26174781', '97613792', '1156006445114171392', '1132020458868609024']
+    CRC_cohesiveness = [0.01349734642883741, -2.788500604640415e-144, 0.0134973464288222, 0.42924528301886794, 2.8]
 
-    # STExa: size 20
-    STExa_community = ['826495641983528960', '714669102', '37831300', '31049466', '1511414336', '1217938840834465798', '1050655603', '1156006445114171392', '1043676244771053568', '101598743', '1026320534647263232', '1092566360771739648', '2649430386', '1151565926350897152', '1099134757', '110801838', '1143262478027186176', '106010466', '1163317745628450818', '1165117868150796288']
-    STExa_cohesiveness = [1.4995193999858507, 1.4995193999854792, 0.29777059622094726, 0.13584078875296696, 5.873684210526315]
+    # STExa: size 30
+    STExa_community = ['826495641983528960', '110801838', '4546645175', '123276343', '1099134757', '1511414336', '1076713315102089217', '106010466', '1163317745628450818', '1043676244771053568', '1092566360771739648', '1143262478027186176', '1026320534647263232', '101598743', '1156006445114171392', '1050655603', '1217938840834465798', '21213366', '116977325', '208322515', '117879582', '31049466', '1151565926350897152', '2649430386', '102807189', '1165117868150796288', '360188566', '37831300', '714669102', '74191033']
+    STExa_cohesiveness = [0.24395339527887888, 5.3074290099383305e-137, 0.23940617079635856, 0.14757969303423848, 3.4482758620689653]
 
     # I2ACSM: score: 0.9779547, parameter: [1, 4, 0.1], for communities with same size, no matter how to change the parameter, the community and cohesiveness score is the same
-    # Among size 8, 9, and 14, choose the smallest one
-    I2ACSM_community = ['1099134757', '1511414336', '1253831120', '826495641983528960', '1156006445114171392', '282949556', '570477589', '2767170695']
-    I2ACSM_cohesiveness = [0.13219483903031704, 0.13219483903031704, 0.125, 0.10501355013550136, 2.767857142857143]
+    # Among size 8, 9, and 14, choose the largest one
+    I2ACSM_community = ['1099134757', '636003798', '1511414336', '284829979', '1253831120', '826495641983528960', '1026320534647263232', '1156006445114171392', '2455289731', '282949556', '331971443', '570477589', '913110838889926784', '2767170695']
+    I2ACSM_cohesiveness = [0.02506650051069805, 5.698594907508587e-159, 0.0250665005106698, 0.16223067173637515, 1.4065934065934067]
 
     # Repeeling: ['50000000', '5000000', '0', '2'], for communities with same size, no matter how to change the parameter, the community and cohesiveness score is the same
-    Repeeling_community = ['101823277', '1026320534647263232', '1092566360771739648', '360188566', '101598743', '119191281', '21213366', '1076713315102089217', '31049466', '102807189', '208322515', '2649430386', '1020328250', '826495641983528960', '117879582', '1165117868150796288', '1142864340', '1050655603', '287286722', '37831300', '106010466', '110801838', '102128701', '1109445494390980608', '1217938840834465798', '116977325', '1099134757', '1304129868', '1156006445114171392']
-    Repeeling_cohesiveness = [1.0469521609682646, 1.0578246129873792, 0.23910855272644574, 0.13970983342289092, 3.8423645320197046]
+    Repeeling_community = ['1156006445114171392', '1217938840834465798', '1050655603', '101823277', '21213366', '37831300', '1099134757', '1020328250', '102128701', '287286722', '117879582', '1165117868150796288', '1076713315102089217', '360188566', '106010466', '1026320534647263232', '31049466', '101598743', '1092566360771739648', '110801838', '102807189', '1304129868', '116977325', '1109445494390980608', '119191281', '826495641983528960', '2649430386', '208322515', '1142864340']
+    Repeeling_cohesiveness = [0.21291206102763036, -6.7535068853255e-06, 0.18852692197912613, 0.13970983342289092, 3.8423645320197046]
 
 
     # TransZero_localsearch: 
-    Local_community = ['1099134757', '1143262478027186176', '1028356298503532555', '1151565926350897152', '1156006445114171392', '1163317745628450818', '1511414336']
-    Local_cohesiveness = [0.1523896587364759, 0.1523896587354136, 0.42857142495909567, 0.011089681774349084, 0.5476190476190477]
+    Local_community = ['1099134757', '1026320534647263232', '1143262478027186176', '1028356298503532555', '1087543962943004800', '1151565926350897152']
+    Local_cohesiveness = [0.058488501191628774, 0.0, 0.058488501191628774, 0.009086050240513094, 0.5666666666666667]
 
 
     # Find the combination of the community nodes in all algorithms
@@ -300,9 +335,9 @@ if __name__ == "__main__":
     """
 
     font_size = 17
-    fig, ax = plt.subplots(figsize=(8, 4))
+    fig, ax = plt.subplots(figsize=(8, 3))
     communities = ['WCF-CRC', 'ST-Exa', 'Repeeling+', 'I2ACSM', 'TransZero_LS']
-    measure_name = ['EL', 'SIT', 'CED', 'GIP', 'GID', r'$k$-core', r'$k$-truss']
+    measure_name = ['EI', 'SIT', 'CED', 'GIP', 'GID', r'$k$-core', r'$k$-truss']
     cohesiveness_scores = [
         CRC_cohesiveness,
         STExa_cohesiveness,
@@ -338,7 +373,7 @@ if __name__ == "__main__":
     ax.set_ylabel('Values', fontsize=font_size)
     ax.set_ysticks = np.linspace(0, 1, num=6)
     ax.set_yticklabels(ax.get_yticks(), fontsize=font_size)
-    ax.legend(loc='best', fontsize=15, ncol=3, columnspacing=0.5)
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.30), fontsize=17, ncol=7, columnspacing=0.5, handlelength=0.9, handletextpad=0.6)
 
     # Show the plot
     plt.tight_layout()
