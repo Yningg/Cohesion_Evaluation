5cf2123391a9f31cc3b2c7786cc515c6e4ed00d3
Yningg <yning.zh@gmail.com>
2025-04-13 15:06:34 +0800

    Update

--- Original_Datasets/Generate_node_mapping.py
+++ Original_Datasets/Generate_node_mapping.py
@@ -1,14 +1,6 @@
 """
-This script is used to generate the node mapping for the original datasets.
+Generate the node mapping for the original datasets.
 """
-
-
-import os
-import pandas as pd
-import numpy as np
-import networkx as nx
-from datetime import datetime
-
 import sys
 target_path = "./"
 sys.path.append(target_path)
@@ -37,13 +29,8 @@ def generate_node_mapping(dataset_name, source_path, target_path):
 if __name__ == "__main__":
     dataset_list = ["BTW17", "Chicago_COVID", "Crawled_Dataset144", "Crawled_Dataset26"]
 
-    # Set for the source path
     source_path = "D:/Cohesion_Evaluation/Original_Datasets/Preprocessed_Datasets/"
-
-     # Set for storing node mapping 
-    target_path_node_mapping = "D:/Cohesion_Evaluation/Original_Datasets/Node_Mapping/"
-    
+    target_path = "D:/Cohesion_Evaluation/Original_Datasets/Node_Mapping/"
 
     for dataset_name in dataset_list:
-        # Given preprocessed dataset, generate its node mapping list first
-        generate_node_mapping(dataset_name, source_path, target_path_node_mapping)
+        generate_node_mapping(dataset_name, source_path, target_path)
